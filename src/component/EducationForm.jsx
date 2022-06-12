f827f4b95fb24ef6f7918019d702048ff94fcdc3
siddharth0-0 <agrawalsid05@gmail.com>
2022-06-12 23:31:33 +0530

    form-design

--- src/component/EducationForm.jsx
+++ src/component/EducationForm.jsx
@@ -1,65 +1,70 @@
+import { useState } from 'react'
 import classes from './EducationForm.module.css'
 
 function EducationForm() {
+
+  const [formValues, setFormValues] = useState([
+    { school: "sam", location: "", degree: "", startMonth: "", endMonth: "" },
+  ])
+
+  const addFormFields = () => {
+    setFormValues([...formValues, { school: "", location: "", degree: "", startMonth: "", endMonth: "" }])
+  }
+
+  const removeFormFields = (i) => {
+    let newForm = [...formValues];
+    newForm.splice(i, 1);
+    setFormValues(newForm)
+  }
+
   return (
-    <div className={classes.formWrap}>
-      <div className={classes.formHead}>
+    <div className='formWrap'>
+      <div className='formHead'>
         <h3>Educational Details</h3>
       </div>
       <form>
-        <div className={classes.mainWrapper}>
-          <div className={classes.rowInputWrapper} >
-            <div className={classes.inputWrapper} >
-              <label htmlFor='school'> School Name</label>
-              <input type="text" required id="school" />
-            </div>
-            <div className={classes.inputWrapper} >
-              <label htmlFor='location'> School Location</label>
-              <input type="text" required id="location" />
-            </div>
-          </div>
-          <div className={classes.longInput} >
-            <label htmlFor='degree'> Degree</label>
-            <input type="text" required id="degree" />
-          </div>
-          <div className={classes.rowInputWrapper} >
-            <div className={classes.inputWrapper} >
-              <label htmlFor="startMonth">Start Month</label>
-              <input type="month" required id="startMonth" />
-            </div>
-            <div className={classes.inputWrapper} >
-              <label htmlFor="endMonth">End Month</label>
-              <input type="month" required id="endMonth" />
-            </div>
-          </div>
-        </div>
+        {formValues.map((item, index) => {
+          return (
+            <div className="mainWrapper" key={index}>
+              <div className='rowInputWrapper' >
+                <div className='shortInput' >
+                  <label htmlFor='school'> School Name</label>
+                  <input type="text" required id="school" />
+                </div>
+                <div className='shortInput' >
+                  <label htmlFor='location'> School Location</label>
+                  <input type="text" required id="location" />
+                </div>
+              </div>
+              <div className='longInput' >
+                <label htmlFor='degree'> Degree</label>
+                <input type="text" required id="degree" />
+              </div>
+              <div className='rowInputWrapper' >
+                <div className='shortInput' >
+                  <label htmlFor="startMonth">Start Month</label>
+                  <input type="month" required id="startMonth" />
+                </div>
+                <div className='shortInput' >
+                  <label htmlFor="endMonth">End Month</label>
+                  <input type="month" required id="endMonth" />
+                </div>
+              </div>
 
-        <div className={classes.mainWrapper}>
-          <div className={classes.rowInputWrapper} >
-            <div className={classes.inputWrapper} >
-              <label htmlFor='school'> School Name</label>
-              <input type="text" required id="school" />
+              <div className={classes.btnWrap}>
+                {
+                  index ?
+                    <button type="button" className='remove' onClick={() => removeFormFields(index)}>Remove</button>
+                    : null
+                }
+                {
+                  formValues.length < 2 ? <button type="button" className='add' onClick={() => addFormFields()}>Add</button> : null
+                }
+
+              </div>
             </div>
-            <div className={classes.inputWrapper} >
-              <label htmlFor='location'> School Location</label>
-              <input type="text" required id="location" />
-            </div>
-          </div>
-          <div className={classes.longInput} >
-            <label htmlFor='degree'> Degree</label>
-            <input type="text" required id="degree" />
-          </div>
-          <div className={classes.rowInputWrapper} >
-            <div className={classes.inputWrapper} >
-              <label htmlFor="startMonth">Start Month</label>
-              <input type="month" required id="startMonth" />
-            </div>
-            <div className={classes.inputWrapper} >
-              <label htmlFor="endMonth">End Month</label>
-              <input type="month" required id="endMonth" />
-            </div>
-          </div>
-        </div>
+          )
+        })}
       </form>
     </div>
   )
--- src/component/ExperienceForm.jsx
+++ src/component/ExperienceForm.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classes from './ExperienceForm.module.css'
 
 function ExperienceForm() {
+
+  const [formValues, setFormValues] = useState([
+    { company: "", job: "", description: "", startMonth: "", endMonth: "" },
+  ])
+
+  const addFormFields = () => {
+    setFormValues([...formValues, { company: "", job: "", description: "", startMonth: "", endMonth: "" }])
+  }
+
+  const removeFormFields = (i) => {
+    let newForm = [...formValues];
+    newForm.splice(i, 1);
+    setFormValues(newForm)
+  }
+
   return (
-    <div>ExperienceForm</div>
+    <div className='formWrap'>
+      <div className='formHead'>
+        <h3>Experience Details</h3>
+      </div>
+      <form>
+        {formValues.map((item, index) => {
+          return (
+            <div className="mainWrapper" key={index}>
+              <div className='rowInputWrapper' >
+                <div className='shortInput' >
+                  <label htmlFor='company'> Company Name</label>
+                  <input type="text" required id="company" />
+                </div>
+                <div className='shortInput' >
+                  <label htmlFor='job'> Job Title</label>
+                  <input type="text" required id="job" />
+                </div>
+              </div>
+
+              <div className='rowInputWrapper' >
+                <div className='shortInput' >
+                  <label htmlFor="startMonth">Start Month</label>
+                  <input type="month" required id="startMonth" />
+                </div>
+                <div className='shortInput' >
+                  <label htmlFor="endMonth">End Month</label>
+                  <input type="month" required id="endMonth" />
+                </div>
+              </div>
+
+              <div className='longInput'>
+                <label htmlFor="description">Description</label>
+                <textarea name="description" cols="30" rows="10" maxLength="250" />
+              </div>
+              
+              <div className={classes.btnWrap}>
+                {
+                  index ?
+                    <button type="button" className='remove' onClick={() => removeFormFields(index)}>Remove</button>
+                    : null
+                }
+                {
+                  formValues.length < 4 ? <button type="button" className='add' onClick={() => addFormFields()}>Add</button> : null
+                }
+
+              </div>
+            </div>
+          )
+        })}
+      </form>
+    </div>
   )
 }
 
